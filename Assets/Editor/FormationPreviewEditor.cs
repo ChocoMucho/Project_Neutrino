d4cc1b849719607f7a5618ef1b9200031ade56c0
ChocoMucho <49467508+ChocoMucho@users.noreply.github.com>
2026-03-21 11:59:47 +0900

    add: 포메이션 편집 에디터 기능 추가
    에디터 상에서 SO의 리스트에 아이템 추가 가능하게 함.
    SO리로드 버튼 추가

--- Assets/Editor/FormationPreviewEditor.cs
+++ Assets/Editor/FormationPreviewEditor.cs
@@ -4,21 +4,52 @@ using UnityEditor;
 [CustomEditor(typeof(FormationPreview))]
 public class FormationPreviewEditor: Editor
 {
+    // SO의 데이터를 수정하기 위한 SerializedObject와 Property
+    private SerializedObject soData;
+    private SerializedProperty pointsProperty;
+
+    private void OnEnable()
+    {
+        // 에디터가 활성화될 때 (오브젝트를 클릭했을 때) 초기화
+        FormationPreview preview = (FormationPreview)target;
+        if (preview.FormationData != null)
+        {
+            InitSerializedObject();
+        }
+    }
+
+    /*SO를 새로 가져와서 직렬화 한다.
+     일종의 새로고침이라고 생각하면 편하다.*/
+    private void InitSerializedObject()
+    {
+        FormationPreview preview = (FormationPreview)target;
+        // SO를 직렬화 객체로 가져옵니다.
+        soData = new SerializedObject(preview.FormationData);
+        // SO 내부의 "points" 리스트 필드를 찾습니다.
+        pointsProperty = soData.FindProperty("points");
+    }
+
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
-
         // target은 Object 형태로 전달돼서 형변환 필요
         FormationPreview preview = (FormationPreview)target;
 
-        if(GUILayout.Button("Add Point"))
+        soData.Update();
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("--- Formation Data Edit ---", EditorStyles.boldLabel);
+
+        EditorGUILayout.PropertyField(pointsProperty, true);
+
+        if (GUILayout.Button("Add Point", GUILayout.Height(25)))
         {
             Undo.RecordObject(preview.FormationData, "Add Point");
             preview.FormationData.points.Add(Vector2.zero);
             EditorUtility.SetDirty(preview.FormationData);
         }
 
-        if(GUILayout.Button("Remove Point"))
+        if(GUILayout.Button("Remove Point", GUILayout.Height(25)))
         {
             Undo.RecordObject(preview.FormationData, "Remove Point");
             if(preview.FormationData.points.Count > 0)
@@ -27,6 +58,13 @@ public class FormationPreviewEditor: Editor
                 EditorUtility.SetDirty(preview.FormationData);
             }
         }
+
+        if (GUILayout.Button("Reload points", GUILayout.Height(25)))
+        {
+            InitSerializedObject();
+        }
+
+        soData.ApplyModifiedProperties();
     }
 
     void OnSceneGUI()
